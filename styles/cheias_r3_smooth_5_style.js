d93b9867fb1d2612b02281149e5f2fb6c63bd74d
Hugo Henrique <hugohcsalis2@gmail.com>
2025-07-06 17:51:49 -0300

    Add files via upload

---
+++ styles/cheias_r3_smooth_5_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_cheias_r3_smooth_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Id") !== null) {
+        labelText = String(feature.get("Id"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(254,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.748}),fill: new ol.style.Fill({color: 'rgba(190,178,151,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
